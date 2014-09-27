62ed7d00e6d0fb774855d094d5c292c8458ca8a1
Jaime <jaime.ngamihi@gmail.com>
2014-09-27 17:35:38 +1200

    added migration and migrated database for image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927023745) do
+ActiveRecord::Schema.define(version: 20140927045901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140927023745) do
     t.string   "gifts_type"
     t.integer  "user_id"
     t.string   "location"
+    t.string   "image"
   end
 
   create_table "requests", force: true do |t|
