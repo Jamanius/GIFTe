4ee803332afc0c581c85d6345dff57260f40c230
Jaime <jaime.ngamihi@gmail.com>
2014-09-27 14:35:30 +1200

    removed leftover carrierwave commands. Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Gift.create(title:"Eggcups", description: "I don't want these, maybe someone else will", comments:"will leave them in the letterbox")
 # Gift.create(title:"Carrots", description: "Yard is overgrown, want to give carrots away", comments:"good for cake")
+
+Gift.create(title:"Carrots", description:"Yummy", location:"Wellington", comments: "Pickup on weekends")
+Gift.create(title:"Horse", descritpion:"Friendly and tall", type:"Useable", location:"Auckland", comments:"likes carrots")
+Gift.create(title:"babysitting", description:"freebaby sitter", type:"Useable", location:"Wellington", comments:"available on weekends")
+Gift.create(title:"shoes", description:"good condition", type:"Wearable", location:"Auckland", comments:"a bit smelly but all good")
