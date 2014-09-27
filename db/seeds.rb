27288cb2a416e65507eafa65631c411220c2cf9b
Jaime <jaime.ngamihi@gmail.com>
2014-09-27 14:43:34 +1200

    fixed seed spelling error

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 # Gift.create(title:"Carrots", description: "Yard is overgrown, want to give carrots away", comments:"good for cake")
 
 Gift.create(title:"Carrots", description:"Yummy", location:"Wellington", comments: "Pickup on weekends")
-Gift.create(title:"Horse", descritpion:"Friendly and tall", type:"Useable", location:"Auckland", comments:"likes carrots")
-Gift.create(title:"babysitting", description:"freebaby sitter", type:"Useable", location:"Wellington", comments:"available on weekends")
-Gift.create(title:"shoes", description:"good condition", type:"Wearable", location:"Auckland", comments:"a bit smelly but all good")
+Gift.create(title:"Horse", description:"Friendly and tall", gifts_type:"Useable", location:"Auckland", comments:"likes carrots")
+Gift.create(title:"babysitting", description:"freebaby sitter", gifts_type:"Useable", location:"Wellington", comments:"available on weekends")
+Gift.create(title:"shoes", description:"good condition", gifts_type:"Wearable", location:"Auckland", comments:"a bit smelly but all good")
