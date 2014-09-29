b3dd2dcc40fd615d37c0b71011de289ec1b5492f
Sleepysaurus <ylegaspi@gmail.com>
2014-09-29 19:47:06 +1300

    putting in map

---
+++ sharpies/ClientSideApp/Models/APIInteractor.js
@@ -0,0 +1,55 @@
+﻿var map;
+var markers = [];
+
+function initialize(geoData) {
+    var myLatlng = { lat: geoData.coords.latitude, lng: geoData.coords.longitude };
+    var mapOptions = {
+        zoom: 13,
+        center: myLatlng
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    google.maps.event.addListener(map, 'click', function (event) {
+        addMarker(event.latLng);
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        draggable:true,
+        title: 'Hello World!'
+    });
+
+
+}
+
+function setUpLocation() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initialize);
+    }
+}
+
+function addMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location,
+        draggable: true,
+        map: map
+    });
+    markers.push(marker);
+}
+
+function setAllMap(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setAllMap(map);
+}
+
+
+google.maps.event.addDomListener(window, 'load', setUpLocation);
+
